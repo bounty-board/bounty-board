47132b343a7f555b6518f4f3e3be752dbdc4d9cc
Bryson Tang <brysontang@gmail.com>
2022-10-18 23:27:26 -0400

    * Moved all web3js code to plugins folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '~/plugins/web3.js',
+    '~/plugins/web3/issue.js',
+    '~/plugins/web3/project.js',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/issue/_id.vue
+++ pages/issue/_id.vue
@@ -1,61 +1,34 @@
 <template>
   <div>
-    <span>Current Bounty: {{ bounty }} - {{ address }}</span>
+    <span>Current Bounty: {{ bounty }} - {{ contractAddress }}</span>
     <button @click="addBounty">Add Bounty</button>
-    <p>{{ coinbase }}</p>
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-import Web3 from 'web3';
-
-import issueAbi from '~/assets/abi/Issue.json';
-
 export default {
   data() {
     return {
       bounty: {},
-      address: '',
+      contractAddress: '',
 
-      web3: null,
-      coinbase: null,
+      value: 100,
     };
-  },
-  async mounted() {
-    if (typeof window.ethereum !== 'undefined') {
-      await window.ethereum.send('eth_requestAccounts');
-      this.web3 = new Web3(window.ethereum);
-
-      // const networkId = await this.web3.eth.net.getId();
-      this.coinbase = await this.web3.eth.getCoinbase();
-      // const balance = await this.web3.eth.getBalance(coinbase);
-      // this.address = this.web3.eth.accounts[0];
-    }
   },
   methods: {
     async addBounty() {
       try {
-        const issue = new this.web3.eth.Contract(issueAbi.abi, this.address);
-        issue.methods.addBounty(100).send({ from: this.coinbase, value: 100 });
+        await this.$addBounty(this.contractAddress, this.value);
       } catch (e) {
         console.log(e);
       }
-
-      // const { id } = this.$route.params;
-      // console.log(this.web3.coinbase);
-      // const { data } = await this.$http.$post(`/api/web3/issue/bounty/${id}`, {
-      //   bounty: 100,
-      //   coinbase: this.web3.coinbase,
-      // });
-      // console.log(data);
     },
   },
   async fetch() {
+    this.$setProjectAddress('0x0a7314dE431F3b2B374741A9cBB20FE97dF3F55B');
     const { id } = this.$route.params;
 
-    const data = await this.$http.$get(`/api/web3/issue/bounty/${id}`);
-    this.bounty = data.bounty;
-    this.address = data.address;
+    this.contractAddress = await this.$getIssueAddress(id);
+    this.bounty = await this.$getBounty(this.contractAddress);
   },
 };
 </script>
